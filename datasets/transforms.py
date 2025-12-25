591772f5d7e86c1416c95cd3bc2a37a854239ac1
Luca Martin <luca@m4rtin.eu>
2025-12-25 14:16:42 +0100

    Add transforms for data augmentation

---
+++ datasets/transforms.py
@@ -0,0 +1,39 @@
+# =======================
+# mandatory coding task 2
+# =======================
+
+from torchvision import transforms
+
+
+def get_train_transform_mild(img_size: int = 64):
+    return transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+    ])
+
+
+def get_train_transform_strong(img_size: int = 64):
+    return transforms.Compose([
+        transforms.RandomResizedCrop(img_size, scale=(0.6, 1.0)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(degrees=20),
+        transforms.ColorJitter(
+            brightness=0.2,
+            contrast=0.2,
+            saturation=0.2,
+            hue=0.1,
+        ),
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.0, 0.3)),
+        transforms.RandomPerspective(distortion_scale=0.5, p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+    ])
