8ae04521dd26e05d8223c9eedc6b5f4421d3fb6d
zweiLuca <luca@m4rtin.eu>
2026-01-02 14:15:26 +0100

    Add feature extractor and yournet

---
+++ models/resnet_feature_extractor.py
@@ -0,0 +1,49 @@
+# =======================
+# mandatory coding task 3
+# =======================
+
+import torch
+import torch.nn as nn
+
+from torchvision.models import resnet18, ResNet18_Weights
+
+
+class ResNet18FeatureExtractor(nn.Module):
+    def __init__(self, pretrained: bool = True):
+        super().__init__()
+
+        if pretrained:
+            backbone = resnet18(weigths=ResNet18_Weights.IMAGENET1K_V1)
+        else:
+            backbone = resnet18(weights=None)
+
+        self.conv1 = backbone.conv1
+        self.bn1 = backbone.bn1
+        self.relu = backbone.relu
+        self.maxpool = backbone.maxpool
+
+        self.layer1 = backbone.layer1
+        self.layer2 = backbone.layer2
+        self.layer3 = backbone.layer3
+        self.layer4 = backbone.layer4
+
+        self.avgpool = backbone.avgpool
+
+        self.out_features = backbone.fc.in_features
+
+    def forward(self, x):
+        # identical to ResNet _forward_impl() until avgpool
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return x
---
+++ models/yournet.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+from models.resnet_feature_extractor import ResNet18FeatureExtractor
+
+
+class YourNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.feature_net = ResNet18FeatureExtractor(pretrained=True)
+
+        self.classifier = torch.nn.Linear(
+            in_features=1024,
+            out_features=num_classes
+        )
+
+    def forward(self, x):
+        x1 = x[:, 0:3, :, :]
+        x2 = x[:, 3:6, :, :]
+
+        f1 = self.feature_net(x1)
+        f2 = self.feature_net(x2)
+
+        fused = torch.cat([f1, f2], dim=1)
+
+        logits = self.classifier(fused)
+
+        return logits
