acd4018b48b8f87e5d182bd8568eaf53106baa4a
zweiLuca <luca@m4rtin.eu>
2026-01-02 15:51:02 +0100

    Fix Typo

--- training/training_loop.py
+++ training/training_loop.py
@@ -6,7 +6,7 @@ from evaluation.eval import evaluate
 from training.train_epoch import train_one_epoch
 
 
-def train_model(model, trian_loader, val_loader, optimizer, device, num_epochs):
+def train_model(model, train_loader, val_loader, optimizer, device, num_epochs):
     history = {
         "train_loss": [],
         "val_acc": [],
@@ -17,7 +17,7 @@ def train_model(model, trian_loader, val_loader, optimizer, device, num_epochs):
     best_state = None
 
     for epoch in range(num_epochs):
-        train_loss = train_one_epoch(model, trian_loader, optimizer, device)
+        train_loss = train_one_epoch(model, train_loader, optimizer, device)
         val_metrics = evaluate(model, val_loader, device)
         val_acc = val_metrics["accuracy"]
         val_tpr = val_metrics["tpr_per_class"]
