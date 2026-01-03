b3f8e6fde457124b53c24492fda46ecc159a0431
zweiLuca <luca@m4rtin.eu>
2026-01-03 15:38:57 +0100

    Update

--- utils/set_seed.py
+++ utils/set_seed.py
@@ -8,6 +8,3 @@ def set_seed(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
